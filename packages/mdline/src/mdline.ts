60d138df43cdf29d7a4d0478b493cb8f61183242
JV-conseil <contact@jv-conseil.net>
2024-03-12 00:56:51 +0100

    dev1

---
+++ packages/mdline/src/mdline.ts
@@ -0,0 +1,13 @@
+import { MdlineParser, MdlineFormatter } from "@mdline/types";
+
+export async function processText(
+    input: string,
+    options: {
+        parser: MdlineParser;
+        formatter: MdlineFormatter;
+    }
+): Promise<string> {
+    const { parser, formatter } = options;
+    const parseResults = parser.parse(input);
+    return formatter.format(parseResults);
+}
