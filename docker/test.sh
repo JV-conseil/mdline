9677e5a7ebfa44dce1699935623937fb24e67b85
JV-conseil <contact@jv-conseil.net>
2024-03-12 00:56:13 +0100

    erase

--- docker/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname $0)"
-docker run  -v $(pwd)/example.md:/example.md mdline /example.md
--- packages/mdline-parser/test/snapshots/endDate/input.md
+++
@@ -1,7 +0,0 @@
-## 2011-01-16--2011-01-20: Range Dates
-
-Start Range Date
-
-## 2012-01-24: OK
-
-OK
